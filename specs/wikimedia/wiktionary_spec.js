db22bbbfa0e30275e1619712821be01596d2fe39
Dan Caseley <dan@caseley.me.uk>
2017-10-30 09:40:30 +0000

    Update

--- specs/wikimedia/wiktionary_spec.js
+++ specs/wikimedia/wiktionary_spec.js
@@ -1,24 +1,36 @@
 const frisby = require('icedfrisby')
 const Joi = require('joi')
 
-describe('Wiktionary', ()=>{
-    it('should define a first word @important', ()=>{
-        frisby.create('Define the word "purple"')
+describe('Wiktionary', function(){
+    it('should define a word @important', function(){
+        frisby.create(this.test.title + ' - Define purple')
             .get('https://en.wiktionary.org/api/rest_v1/page/definition/purple')
             .expectStatus(200)
+            .after(function(error,response){
+                frisby.create(this.test.title + ' - Define violet')
+                    .get('https://en.wiktionary.org/api/rest_v1/page/definition/violet')
+
+            })
         .toss()
     })
 
-    it('should define a second word', ()=>{
-        frisby.create('Define the word "orange"')
+    it('should define a second word', function(){
+        frisby.create(this.test.title)
             .get('https://en.wiktionary.org/api/rest_v1/page/definition/orange')
             .expectStatus(200)
         .toss()
     })
 
-    it('should define a third word @important', ()=>{
+    it('should define a third word', function(){
         frisby.create('Define the word "important"')
             .get('https://en.wiktionary.org/api/rest_v1/page/definition/important')
+            .expectStatus(200)
+        .toss()
+    })
+
+    it('should define a fourth word', function(){
+        frisby.create(this.test)
+            .get('https://en.wiktionary.org/api/rest_v1/page/definition/word')
             .expectStatus(200)
         .toss()
     })
